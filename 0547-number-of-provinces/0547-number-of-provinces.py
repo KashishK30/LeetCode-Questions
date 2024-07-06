2303de5aaa158c3c1364a65ef1b92b3cd2813cb7
KashishK30 <khobragadekashish@gmail.com>
2024-07-06 13:05:38 +0530

    Time: 195 ms (18.74%), Space: 17.3 MB (70.51%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,18 +1,30 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        def dfs(city:int):
-            for neighbour, connected in enumerate(isConnected[city]):
-                if connected == 1 and not visited[neighbour]:
-                    visited[neighbour] = True
-                    dfs(neighbour)
+        def find(parent, i):
+            if parent[i] != i:
+                parent[i] = find(parent, parent[i])
+            return parent[i]
+        
+        def union(parent, rank, x, y):
+            rootX = find(parent, x)
+            rootY = find(parent, y)
+
+            if rank[rootX] > rank[rootY]:
+                parent[rootY] = rootX
+            elif rank[rootY] > rank[rootX]:
+                parent[rootX] = rootY
+            else:
+                parent[rootX] =rootY
+                rank[rootY] += 1
 
         n = len(isConnected)
-        visited = [False] * n
-        num_provinces = 0
+        parent = [i for i in range(n)]
+        rank = [0] * n
 
-        for city in range(n):
-            if not visited[city]:
-                visited[city] = True
-                dfs(city)
-                num_provinces += 1
-        return num_provinces
+        for i in range(n):
+            for j in range(i + 1, n):
+                if isConnected[i][j] == 1:
+                    union(parent, rank, i, j)
+
+        province_count = len(set(find(parent, i) for i in range(n)))
+        return province_count
