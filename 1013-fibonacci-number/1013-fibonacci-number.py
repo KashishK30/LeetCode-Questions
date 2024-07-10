ef299b74191a5993b257ea12517f4449342c40dc
KashishK30 <khobragadekashish@gmail.com>
2024-07-10 22:14:55 +0530

    Time: 31 ms (86.86%), Space: 16.5 MB (12.29%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,10 +1,11 @@
 class Solution:
-    # Iterative approach (Bottom-Up)
+    # Space Optimization
     def fib(self, n: int) -> int:
         if n <= 1:
             return n
-        fibo = [0] * (n + 1)
-        fibo[1] = 1
-        for i in range(2, n + 1):
-            fibo[i] = fibo[i - 1] + fibo[i - 2]
-        return fibo[n]
+
+        a = 0
+        b = 1
+        for _ in range(2, n + 1):
+            a, b = b , a + b
+        return b
