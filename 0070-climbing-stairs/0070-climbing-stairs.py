ec3d4df8893c13f155bbf0f2ff6ce7bf16e53531
KashishK30 <khobragadekashish@gmail.com>
2024-07-10 22:45:19 +0530

    Time: 29 ms (87.68%), Space: 16.3 MB (91.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,9 +4,8 @@ class Solution:
             return 1
         if n == 1:
             return 1
-        fib = [0] * (n + 1)
-        fib[0] = 1
-        fib[1] = 1
-        for i in range(2, n + 1):
-            fib[i] = fib[i - 1] + fib[i - 2]
-        return fib[n]
+        a = 1
+        b = 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
