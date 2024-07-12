766ed659d38f3c5768c17812bd7ace1782840049
KashishK30 <khobragadekashish@gmail.com>
2024-07-12 20:19:28 +0530

    Time: 42 ms (72.26%), Space: 16.5 MB (71.46%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -3,25 +3,30 @@ class Solution:
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
         
-        if obstacleGrid[0][0] == 1:
+        if obstacleGrid[0][0] == 1 or obstacleGrid[m - 1][n - 1] == 1:
             return 0
         
-        dp = [[0] * n for _ in range(m)]
+        obstacleGrid[0][0] = 1 if obstacleGrid[0][0] == 0 else 0
 
-        dp[0][0] = 1
 
         for i in range(1, m):
             if obstacleGrid[i][0] == 0:
-                dp[i][0] = dp[i - 1][0]
+                obstacleGrid[i][0] = obstacleGrid[i - 1][0]
+            else:
+                obstacleGrid[i][0] = 0
 
         for j in range(1, n):
             if obstacleGrid[0][j] == 0:
-                dp[0][j] = dp[0][j - 1]
+                obstacleGrid[0][j] = obstacleGrid[0][j - 1]
+            else:
+                obstacleGrid[0][j] = 0
 
         for i in range(1, m):
             for j in range(1, n):
                 if obstacleGrid[i][j] == 0:
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1]
+                else:
+                    obstacleGrid[i][j] = 0
         
-        return dp[m - 1][n - 1]
+        return obstacleGrid[m - 1][n - 1]
                 
