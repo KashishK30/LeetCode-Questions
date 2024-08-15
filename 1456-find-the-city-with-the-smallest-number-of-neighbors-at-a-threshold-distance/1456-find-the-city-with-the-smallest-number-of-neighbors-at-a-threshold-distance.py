e0125f16e8ecb66a8b4e09d4b0f4bc9bc7b8df7e
KashishK30 <khobragadekashish@gmail.com>
2024-08-15 20:25:25 +0530

    Time: 449 ms (37.13%), Space: 17.6 MB (88.1%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,31 +1,30 @@
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
-        distance = [[float('inf')] * n for _ in range(n)]
+        dist = [[float('inf')] * n for _ in range(n)]
 
-        for i in range(n):
-            distance[i][i] = 0
-        
         for u, v, w in edges:
-            distance[u][v] = w
-            distance[v][u] = w
-        
+            dist[u][v] = w
+            dist[v][u] = w
+            
+        for i in range(n):
+            dist[i][i] = 0
+
         for k in range(n):
             for i in range(n):
                 for j in range(n):
-                    if distance[i][j] > distance[i][k] + distance[k][j]:
-                        distance[i][j] = distance[i][k] + distance[k][j]
-        
-        min_reachable = n
-        best_city = -1
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+
+        min_count = float('inf')
+        result_city = -1
 
         for i in range(n):
-            reachable_count = 0
+            count = 0
             for j in range(n):
-                if distance[i][j] <= distanceThreshold:
-                    reachable_count += 1
+                if dist[i][j] <= distanceThreshold:
+                    count += 1
+
+            if count <= min_count:
+                min_count = count
+                result_city = i
         
-            if reachable_count <= min_reachable:
-                min_reachable = reachable_count
-                best_city = i
-        
-        return best_city
+        return result_city
