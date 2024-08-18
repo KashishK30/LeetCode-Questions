9fa465f6fe615552952f78b1ed0bf9a0f6973919
KashishK30 <khobragadekashish@gmail.com>
2024-08-18 20:32:36 +0530

    Time: 145 ms (60.31%), Space: 17.2 MB (33.13%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -1,20 +1,23 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        n =  len(graph)
-        color = [-1] * n
+        # Bipartite DFS
 
-        for start in range(n):
-            if color[start] == -1:
-                queue = deque([start])
-                color[start] = 0
+        color = [-1] * len(graph)
 
-                while queue:
-                    node = queue.popleft()
-                    for neighbour in graph[node]:
-                        if color[neighbour] == -1:
-                            color[neighbour] = 1 - color[node]
-                            queue.append(neighbour)
-                        elif color[neighbour] == color[node]:
-                            return False
-            
+        def dfs(node, c):
+            color[node] = c
+
+            for neighbor in graph[node]:
+                if color[neighbor] == -1:
+                    if not dfs(neighbor, 1 - c):
+                        return False
+                elif color[neighbor] == color[node]:
+                    return False
+            return True
+
+        for i in range(len(graph)):
+            if color[i] == -1:
+                if not dfs(i, 0):
+                    return False
         return True
+                
