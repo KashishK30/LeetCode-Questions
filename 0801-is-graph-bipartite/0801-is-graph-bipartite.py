fbe5876c6eb62d667684896e6fe89e93ef2c9858
KashishK30 <khobragadekashish@gmail.com>
2024-08-18 20:46:06 +0530

    Time: 143 ms (71.43%), Space: 16.9 MB (89.55%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -1,23 +1,55 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        # Bipartite DFS
+        ## Bipartite BFS
+        n = len(graph)
+        color = [-1] * n
 
-        color = [-1] * len(graph)
+        def bfs(start):
+            queue = deque([start])
 
-        def dfs(node, c):
-            color[node] = c
+            color[start] = 0
 
-            for neighbor in graph[node]:
-                if color[neighbor] == -1:
-                    if not dfs(neighbor, 1 - c):
+            while queue:
+                node = queue.popleft()
+
+                for neighbor in graph[node]:
+                    if color[neighbor] == -1:
+                        color[neighbor] = color[node] + 1
+                        queue.append(neighbor)
+                    elif color[neighbor] == color[node]:
                         return False
-                elif color[neighbor] == color[node]:
-                    return False
             return True
 
-        for i in range(len(graph)):
+        for i in range(n):
             if color[i] == -1:
-                if not dfs(i, 0):
+                if not bfs(i):
                     return False
         return True
+        
+        # TC: O(V + E)
+        # SC: O(V)
+
+        ## Bipartite DFS
+
+        # color = [-1] * len(graph)
+
+        # def dfs(node, c):
+        #     color[node] = c
+
+        #     for neighbor in graph[node]:
+        #         if color[neighbor] == -1:
+        #             if not dfs(neighbor, 1 - c):
+        #                 return False
+        #         elif color[neighbor] == color[node]:
+        #             return False
+        #     return True
+
+        # for i in range(len(graph)):
+        #     if color[i] == -1:
+        #         if not dfs(i, 0):
+        #             return False
+        # return True
+
+        # TC: O(N)
+        # SC: O(1)
                 
