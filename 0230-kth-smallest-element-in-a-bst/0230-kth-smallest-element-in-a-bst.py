df9ca010baadfaa2fb18aeb106adec9d067a3e87
KashishK30 <khobragadekashish@gmail.com>
2024-08-21 12:25:22 +0530

    Time: 55 ms (5.66%), Space: 19.6 MB (5.11%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,18 +6,33 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        ## Iterative
-        stack = []
-        current = root
-        count = 0
+        ## Recurssive
 
-        while current or stack:
-            while current:
-                stack.append(current)
-                current = current.left
-            current = stack.pop()
-            count += 1
-            if count == k:
-                return current.val
-            current = current.right
-        return -1
+        def in_order_traversal(node):
+            if node is None:
+                return []
+
+            left_values = in_order_traversal(node.left)
+
+            current_value = [node.val]
+
+            right_values = in_order_traversal(node.right)
+            return left_values + current_value + right_values
+        sorted_values = in_order_traversal(root)
+        return sorted_values[k - 1]
+
+        ## Iterative
+        # stack = []
+        # current = root
+        # count = 0
+
+        # while current or stack:
+        #     while current:
+        #         stack.append(current)
+        #         current = current.left
+        #     current = stack.pop()
+        #     count += 1
+        #     if count == k:
+        #         return current.val
+        #     current = current.right
+        # return -1
