8a21d813333dc978ed03e806951f2474a930a770
KashishK30 <khobragadekashish@gmail.com>
2024-08-22 19:22:56 +0530

    Time: 624 ms (31.97%), Space: 24.9 MB (89.61%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,14 +1,39 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        ## Memoization
         m = len(text1)
         n = len(text2)
+        dp = [[-1] * (n + 1) for _ in range(m + 1)]
 
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        def lcs_memoization(text1, text2, m, n, dp):
+            if m == 0 or n == 0:
+                return 0
+
+            if dp[m][n] != -1:
+                return dp[m][n]
+
+            if text1[m - 1] == text2[n - 1]:
+                dp[m][n] = 1 + lcs_memoization(text1, text2, m - 1, n - 1, dp)
+            else:
+                dp[m][n] = max(lcs_memoization(text1, text2, m - 1, n, dp), lcs_memoization(text1, text2, m, n - 1, dp))
+            return dp[m][n]
         
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                if text1[i - 1] == text2[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1] + 1
-                else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-        return dp[m][n]
+        return lcs_memoization(text1, text2, m, n, dp)
+            
+
+        ## Recursion 
+
+        # def lcs_recursive(text1, tex2, m, n):
+        #     if m == 0 or n == 0:
+        #         return 0
+        #     elif text1[m - 1] == text2[n - 1]:
+        #         return 1 + lcs_recursive(text1, text2, m - 1, n - 1)
+        #     else:
+        #         return max(lcs_recursive(text1, text2, m - 1, n), lcs_recursive(text1, text2, m, n - 1))
+
+        # return lcs_recursive(text1, text2, len(text1), len(text2))
+        ## TC: 2**(n + m)
+
+        ## Brute Force
+        # O(2^N)
+
