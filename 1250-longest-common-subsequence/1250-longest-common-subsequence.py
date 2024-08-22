4dd96748ea726179cb0a06de10468ae4656d8d87
KashishK30 <khobragadekashish@gmail.com>
2024-08-22 19:27:43 +0530

    Time: 469 ms (69.96%), Space: 41.7 MB (69.32%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,24 +1,41 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        ## Memoization
+        ## Tabulation
         m = len(text1)
         n = len(text2)
-        dp = [[-1] * (n + 1) for _ in range(m + 1)]
 
-        def lcs_memoization(text1, text2, m, n, dp):
-            if m == 0 or n == 0:
-                return 0
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-            if dp[m][n] != -1:
-                return dp[m][n]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+        return dp[m][n]
 
-            if text1[m - 1] == text2[n - 1]:
-                dp[m][n] = 1 + lcs_memoization(text1, text2, m - 1, n - 1, dp)
-            else:
-                dp[m][n] = max(lcs_memoization(text1, text2, m - 1, n, dp), lcs_memoization(text1, text2, m, n - 1, dp))
-            return dp[m][n]
+        ## Memoization
+        # m = len(text1)
+        # n = len(text2)
+        # dp = [[-1] * (n + 1) for _ in range(m + 1)]
+
+        # def lcs_memoization(text1, text2, m, n, dp):
+        #     if m == 0 or n == 0:
+        #         return 0
+
+        #     if dp[m][n] != -1:
+        #         return dp[m][n]
+
+        #     if text1[m - 1] == text2[n - 1]:
+        #         dp[m][n] = 1 + lcs_memoization(text1, text2, m - 1, n - 1, dp)
+        #     else:
+        #         dp[m][n] = max(lcs_memoization(text1, text2, m - 1, n, dp), lcs_memoization(text1, text2, m, n - 1, dp))
+        #     return dp[m][n]
         
-        return lcs_memoization(text1, text2, m, n, dp)
+        # return lcs_memoization(text1, text2, m, n, dp)
+
+        ## TC: O(m * n)
+        ## SC: O(m * n)
             
 
         ## Recursion 
