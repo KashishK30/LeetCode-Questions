4c9eadceb72d7bd5787f150be3cead7551bda9b8
KashishK30 <khobragadekashish@gmail.com>
2024-09-06 22:00:18 +0530

    Time: 147 ms (87.48%), Space: 16.5 MB (99.6%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -5,17 +5,43 @@ class Solution:
         # Add all the chars to the lcs which are not part of the word2
         # m - lcs
         # Total = (n - lcs) + (m - lcs) = (n + m) - (2 * lcs)
+
+        # Space Optimization
+
         m = len(word1)
         n = len(word2)
 
-        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        prev = [0] * (n + 1)
+        curr = [0] * (n + 1)
 
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 if word1[i - 1] == word2[j - 1]:
-                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                    curr[j] = 1 + prev[j - 1]
                 else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-        lcs = dp[m][n]
+                    curr[j] = max(prev[j], curr[j - 1])
+            prev = curr[:]
+        lcs = prev[n]
 
-        return (n + m) - (2 * lcs)
+        return (m + n) - (2 * lcs)
+
+
+        # # Tabulation
+
+        # m = len(word1)
+        # n = len(word2)
+
+        # dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+
+        # for i in range(1, m + 1):
+        #     for j in range(1, n + 1):
+        #         if word1[i - 1] == word2[j - 1]:
+        #             dp[i][j] = 1 + dp[i - 1][j - 1]
+        #         else:
+        #             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        # lcs = dp[m][n]
+
+        # return (n + m) - (2 * lcs)
+
+        # # TC: O(m * n)
+        # # SC: O(m * n)
