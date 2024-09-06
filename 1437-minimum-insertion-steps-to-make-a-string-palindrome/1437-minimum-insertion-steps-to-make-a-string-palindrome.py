a450be6e9462cdedc3b1e371197f12ede573540d
KashishK30 <khobragadekashish@gmail.com>
2024-09-06 21:39:46 +0530

    Time: 597 ms (59.5%), Space: 16.5 MB (97.79%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,20 +1,44 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
+        # Space Optimization
         n = len(s)
         s1 = s
         s2 = s[::-1]
-        dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
-        
+
+        prev = [0] * (n + 1)
+        curr = [0] * (n + 1)
+
         for i in range(1, n + 1):
             for j in range(1, n + 1):
                 if s1[i - 1] == s2[j - 1]:
-                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                    curr[j] = prev[j - 1] + 1
                 else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                    curr[j] = max(prev[j], curr[j - 1])
+            prev = curr[:]
+        max_common_len_sub = prev[n]
 
-        max_len_sub = dp[n][n]
+        return n - max_common_len_sub
 
-        return n - max_len_sub
+        # # Tabulation
+
+        # n = len(s)
+        # s1 = s
+        # s2 = s[::-1]
+        # dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+        
+        # for i in range(1, n + 1):
+        #     for j in range(1, n + 1):
+        #         if s1[i - 1] == s2[j - 1]:
+        #             dp[i][j] = 1 + dp[i - 1][j - 1]
+        #         else:
+        #             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        # max_len_sub = dp[n][n]
+
+        # return n - max_len_sub
+
+        # # TC: O(n * n)
+        # # SC: O(n * n)
 
 
 
