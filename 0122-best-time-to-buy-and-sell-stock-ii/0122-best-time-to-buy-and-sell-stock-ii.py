f5381c667c7a7f5ccb3fe1269a9dcc726c3ca8fb
KashishK30 <khobragadekashish@gmail.com>
2024-09-07 18:59:20 +0530

    Time: 69 ms (8.63%), Space: 17.9 MB (12.66%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,28 +1,51 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
-        # Tabulation
+        # Space Optimization
         n = len(prices)
         if n == 0:
             return 0
         
-        dp = [[-1 for _ in range(2)] for _ in range(n + 1)]
+        ahead = [0, 0]
+        curr = [0, 0]
 
-        dp[0][0] = 0
+        ahead[0] = ahead[1] = 0
 
-        for j in range(2):
-            dp[n][j] = 0
-        
-        profit = 0
         for i in range(n - 1, -1, -1):
             for j in range(2):
-                if j == 0:
-                    profit = max(-prices[i] + dp[i + 1][1], 0 + dp[i + 1][0])
-                elif j == 1:
-                    profit = max(+prices[i] + dp[i + 1][0], 0 + dp[i + 1][1])
-                dp[i][j] = profit
-                
-        return dp[0][0]
+                profit = 0
+
+                if j == 0: # Can buy
+                    profit = max(-prices[i] + ahead[1], 0 + ahead[0])
+                elif j == 1: # Can sell
+                    profit = max(+prices[i] + ahead[0], 0 + ahead[1])
+                curr[j] = profit
+            curr, ahead = ahead, curr
+        return ahead[0]
+
+        # # Tabulation
+        # n = len(prices)
+        # if n == 0:
+        #     return 0
+        
+        # dp = [[-1 for _ in range(2)] for _ in range(n + 1)]
+
+        # dp[0][0] = 0
+
+        # for j in range(2):
+        #     dp[n][j] = 0
+        
+        # profit = 0
+        # for i in range(n - 1, -1, -1):
+        #     for j in range(2):
+        #         if j == 0:
+        #             profit = max(-prices[i] + dp[i + 1][1], 0 + dp[i + 1][0])
+        #         elif j == 1:
+        #             profit = max(+prices[i] + dp[i + 1][0], 0 + dp[i + 1][1])
+        #         dp[i][j] = profit
+
+        # return dp[0][0]
+        # # TC: O(n * 2)
+        # # SC: O(n * 2)
 
         # # Memoization
 
