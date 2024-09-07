984408a7a1adaacd3a6a717722535142446285d4
KashishK30 <khobragadekashish@gmail.com>
2024-09-07 18:50:13 +0530

    Time: 65 ms (16.68%), Space: 20.1 MB (5.3%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,31 +1,59 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
 
+        # Tabulation
+        n = len(prices)
         if n == 0:
             return 0
+        
+        dp = [[-1 for _ in range(2)] for _ in range(n + 1)]
 
-        dp = [[-1 for _ in range(2)] for _ in range(n)]
+        dp[0][0] = 0
 
-        def memoization(ind, buy):
+        for j in range(2):
+            dp[n][j] = 0
+        
+        profit = 0
+        for i in range(n - 1, -1, -1):
+            for j in range(2):
+                if j == 0:
+                    profit = max(-prices[i] + dp[i + 1][1], 0 + dp[i + 1][0])
+                elif j == 1:
+                    profit = max(+prices[i] + dp[i + 1][0], 0 + dp[i + 1][1])
+                dp[i][j] = profit
+                
+        return dp[0][0]
+
+        # # Memoization
+
+        # n = len(prices)
+
+        # if n == 0:
+        #     return 0
+
+        # dp = [[-1 for _ in range(2)] for _ in range(n)]
+
+        # def memoization(ind, buy):
             
-            if ind == n:
-                return 0
+        #     if ind == n:
+        #         return 0
 
-            if dp[ind][buy] != -1:
-                return dp[ind][buy]
+        #     if dp[ind][buy] != -1:
+        #         return dp[ind][buy]
 
-            profit = 0
+        #     profit = 0
 
-            if buy == 0:
-                profit = max( -prices[ind] + memoization(ind + 1, 1), 
-                0 + memoization(ind + 1, 0))
-            elif buy == 1:
-                profit = max( +prices[ind] + memoization(ind + 1, 0),
-                0 + memoization(ind + 1, 1))
+        #     if buy == 0:
+        #         profit = max( -prices[ind] + memoization(ind + 1, 1), 
+        #         0 + memoization(ind + 1, 0))
+        #     elif buy == 1:
+        #         profit = max( +prices[ind] + memoization(ind + 1, 0),
+        #         0 + memoization(ind + 1, 1))
 
-            dp[ind][buy] = profit
-            return profit
+        #     dp[ind][buy] = profit
+        #     return profit
 
-        return memoization(0, 0)
+        # return memoization(0, 0)
 
+        # # TC: O(n * 2)
+        # # SC: O(n * 2) + O(n)
