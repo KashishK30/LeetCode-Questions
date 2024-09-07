0bf7421d0a4e6b7ba9591e1f3c27ccc3d9f32a6d
KashishK30 <khobragadekashish@gmail.com>
2024-09-07 16:27:15 +0530

    Time: 330 ms (69.08%), Space: 16.6 MB (89.92%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,38 +1,70 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        # Tabulation
+        # Detailed comments are given in the tabulation code
+        
+        # Space Optimization
         m = len(s)
         n = len(p)
 
-        # Create a dp array of size (m + 1) * (n + 1)
-        # dp[i][j] gives True if s[0: m] matches withp[0: n], False otherwise
-        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        prev = [False] * (n + 1)
+        curr = [False] * (n + 1)
 
-        # Base Case: Both string and pattern are empty, so they match
-        dp[0][0] = True
+        prev[0] = True
 
-        # Base Case: if string is empty, pattern can only match if it contains only "*"
         for j in range(1, n + 1):
             if p[j - 1] == "*":
-                dp[0][j] = dp[0][j - 1] # "*" can match an empty string by acting as an empty string
-            else:
-                dp[0][j] = False # if there is a non "*" char in the pattern, it cannot match the empty string
-        
-        # Fill the table for i > 0 and j > 0
+                prev[j] = prev[j - 1]
+
         for i in range(1, m + 1):
+            curr[0] = False
+
             for j in range(1, n + 1):
-                # Case1: if chars match or if pattern has "?" it can macth a single char
                 if s[i - 1] == p[j - 1] or p[j - 1] == "?":
-                    dp[i][j] = dp[i - 1][j - 1]
-                # Case 2: if charss do not match and pattern has "*", it can match the sequence of chars
+                    curr[j] = prev[j - 1]
                 elif p[j - 1] == "*":
-                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
-                # Case 3: if chars do not match and doesn't have "*' or "?"
-                # it's already False
-              
-        return dp[m][n]
+                    curr[j] = prev[j] or curr[j - 1]
+                else:
+                    curr[j] = False
+            prev, curr = curr, prev
+        return prev[n]
         # TC: O(m * n)
-        # SC: O(m * n)
+        # SC: O(n)
+
+        # # Tabulation
+        # m = len(s)
+        # n = len(p)
+
+        # # Create a dp array of size (m + 1) * (n + 1)
+        # # dp[i][j] gives True if s[0: m] matches withp[0: n], False otherwise
+        # dp = [[False] * (n + 1) for _ in range(m + 1)]
+
+        # # Base Case: Both string and pattern are empty, so they match
+        # dp[0][0] = True
+
+        # # Base Case: if string is empty, pattern can only match if it contains only "*"
+        # for j in range(1, n + 1):
+        #     if p[j - 1] == "*":
+        #         dp[0][j] = dp[0][j - 1] # "*" can match an empty string by acting as an empty string
+        #     else:
+        #         dp[0][j] = False # if there is a non "*" char in the pattern, it cannot match the empty string
+        
+        # # Fill the table for i > 0 and j > 0
+        # for i in range(1, m + 1):
+        #     for j in range(1, n + 1):
+        #         # Case1: if chars match or if pattern has "?" it can macth a single char
+        #         if s[i - 1] == p[j - 1] or p[j - 1] == "?":
+        #             dp[i][j] = dp[i - 1][j - 1]
+        #         # Case 2: if chars do not match and pattern has "*", it can match the sequence of chars
+        #         elif p[j - 1] == "*":
+        #             # 1. "*": Act as an empty string (match s[0 : i] to p[0 : j - 1])
+        #             # 2. "*": mactches one or more chars, so we check s[0: i - 1] to p[0: j]
+        #             dp[i][j] = dp[i - 1][j] or dp[i][j - 1] # 0 or more chars
+        #         # Case 3: if chars do not match and doesn't have "*' or "?"
+        #         # it's already False
+              
+        # return dp[m][n]
+        # # TC: O(m * n)
+        # # SC: O(m * n)
 
         # # Memoization
 
