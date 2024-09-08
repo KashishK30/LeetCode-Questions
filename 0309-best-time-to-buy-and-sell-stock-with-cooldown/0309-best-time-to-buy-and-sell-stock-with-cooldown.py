ace8687fc7f8c64d292e7bea5d3007a450b8c545
KashishK30 <khobragadekashish@gmail.com>
2024-09-08 22:11:06 +0530

    Time: 42 ms (72.26%), Space: 16.7 MB (91.94%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,16 +1,35 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-    # Tabulation
+    # Space Optimization
         n = len(prices)
-        dp = [[0 for _ in range(2)] for _ in range(n + 2)]
+        ahead = [0, 0]
+        curr = [0, 0]
+        next_to_next = [0, 0]
 
         for ind in range(n - 1, -1, -1):
             for buy in range(2):
                 if buy == 0:
-                    dp[ind][buy] = max(-prices[ind] + dp[ind + 1][1], 0 + dp[ind + 1][0])
+                    curr[buy] = max( - prices[ind] + ahead[1], 0 + ahead[0])
                 else:
-                    dp[ind][buy] = max(+prices[ind] + dp[ind + 2][0], 0 + dp[ind + 1][1])
-        return dp[0][0]
+                    curr[buy] = max(+ prices[ind] + next_to_next[0], 0 + ahead[1])
+            next_to_next = ahead[:]
+            ahead = curr[:]
+            
+        return ahead[0]
+
+    # # Tabulation
+    #     n = len(prices)
+    #     dp = [[0 for _ in range(2)] for _ in range(n + 2)]
+
+    #     for ind in range(n - 1, -1, -1):
+    #         for buy in range(2):
+    #             if buy == 0:
+    #                 dp[ind][buy] = max(-prices[ind] + dp[ind + 1][1], 0 + dp[ind + 1][0])
+    #             else:
+    #                 dp[ind][buy] = max(+prices[ind] + dp[ind + 2][0], 0 + dp[ind + 1][1])
+    #     return dp[0][0]
+    # # TC: O(n * 2)
+    # # SC: O(n * 2)
 
     ## Memoization
 
