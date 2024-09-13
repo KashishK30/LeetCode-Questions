1487a30d15d4d1ab6ff08dba90a1e33eb9482da8
KashishK30 <khobragadekashish@gmail.com>
2024-09-13 18:54:13 +0530

    Time: 292 ms (62.21%), Space: 16.6 MB (85.13%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.py
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.py
@@ -1,24 +1,45 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
-        # Mmeoization
+        # Tabulation
         n = len(arr)
-        dp = [-1] * (n)
+        dp = [0] * (n + 1)
 
-        def memoization(ind):
-            if ind == n:
-                return 0
-            
-            if dp[ind] != -1:
-                return dp[ind]
-            
+        for ind in range(n - 1, -1, -1):
             max_val = float('-inf')
             max_sum = 0
             for i in range(ind, min(ind + k, n)):
                 max_val = max(max_val, arr[i])
-                current_sum = (max_val * (i - ind + 1)) + memoization(i + 1)
-                max_sum = max(current_sum, max_sum)
+                current_val = max_val * (i - ind + 1) + dp[i + 1]
+                max_sum = max(max_sum, current_val)
             dp[ind] = max_sum
-            return dp[ind]
+        return dp[0]
+
+        # TC: O(n * n)
+        # SC: O(n)
+
+
+        # # Memeoization
+        # n = len(arr)
+        # dp = [-1] * (n)
+
+        # def memoization(ind):
+        #     if ind == n:
+        #         return 0
+            
+        #     if dp[ind] != -1:
+        #         return dp[ind]
+            
+        #     max_val = float('-inf')
+        #     max_sum = 0
+        #     for i in range(ind, min(ind + k, n)):
+        #         max_val = max(max_val, arr[i])
+        #         current_sum = (max_val * (i - ind + 1)) + memoization(i + 1)
+        #         max_sum = max(current_sum, max_sum)
+        #     dp[ind] = max_sum
+        #     return dp[ind]
+
+        # # TC: O(n * n)
+        # # SC: O(n) + O(n) [Auxiliary stack space]
 
         return memoization(0)
 
