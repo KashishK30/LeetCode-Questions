93d61facb8ace8e8f9ac0672f23af2382259ed72
KashishK30 <khobragadekashish@gmail.com>
2024-09-13 18:46:00 +0530

    Time: 353 ms (35.63%), Space: 17.6 MB (26.3%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.py
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.py
@@ -1,13 +1,43 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        # Mmeoization
         n = len(arr)
-        dp = [0] * (n + 1)
+        dp = [-1] * (n)
 
-        for i in range(1, n + 1):
-            max_val = 0
-            for j in range(1, min(i, k) + 1):
-                max_val = max(max_val, arr[i - j])
-                dp[i] = max(dp[i], dp[i - j] + max_val * j)
-        return dp[n]
+        def memoization(ind):
+            if ind == n:
+                return 0
+            
+            if dp[ind] != -1:
+                return dp[ind]
+            
+            max_val = float('-inf')
+            max_sum = 0
+            for i in range(ind, min(ind + k, n)):
+                max_val = max(max_val, arr[i])
+                current_sum = (max_val * (i - ind + 1)) + memoization(i + 1)
+                max_sum = max(current_sum, max_sum)
+            dp[ind] = max_sum
+            return dp[ind]
 
+        return memoization(0)
+
+        # # Recursion
+
+        # def recursion(ind):
+        #     if ind == len(arr):
+        #         return 0
+            
+        #     max_val = float('-inf')
+        #     max_sum = 0
+        #     for i in range(ind, min(ind + k, len(arr))):
+        #         max_val = max(max_val, arr[i])
+        #         current_sum = max_val * (i - ind + 1) + recursion(i + 1)
+        #         max_sum = max(max_sum, current_sum)
+
+        #     return max_sum
+
+        # return recursion(0)
+
+        # # TC: Exponential
 
