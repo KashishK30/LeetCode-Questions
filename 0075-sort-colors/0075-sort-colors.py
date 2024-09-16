f608721ba194fc1cd795ef3cd8157c7383951de7
KashishK30 <khobragadekashish@gmail.com>
2024-09-16 13:09:33 +0530

    Time: 37 ms (58.46%), Space: 16.4 MB (67.96%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,24 +3,42 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-    # SELECTION SORT
-        n = len(nums)
-        for i in range(n):
-            min_ind = i
-            for j in range(i + 1, n):
-                if nums[j] < nums[min_ind]:
-                    min_ind = j
+    # INSERTION SORT
+        for i in range(1, len(nums)):
+            key = nums[i]
+            j = i - 1
+            while j >= 0 and key < nums[j]:
+                nums[j + 1] = nums[j]
+                j -= 1
+            nums[j + 1] = key
 
-            nums[i], nums[min_ind] = nums[min_ind], nums[i]
         return nums
 
+
+    # # SELECTION SORT
+
+    #     n = len(nums)
+    #     for i in range(n):
+    #         min_ind = i
+    #         for j in range(i + 1, n):
+    #             if nums[j] < nums[min_ind]:
+    #                 min_ind = j
+
+    #         nums[i], nums[min_ind] = nums[min_ind], nums[i]
+    #     return nums
+
+    # # TC: O(n ^ 2)
+    # # SC: O(1)
+
     # # BUBBLE SORT
+
     #     n = len(nums)
     #     for i in range(n):
     #         for j in range(n - i - 1):
     #             if nums[j] > nums[j + 1]:
     #                 nums[j], nums[j + 1] = nums[j + 1], nums[j]
     #     return nums
+
     # # TC: O(n ^ 2)
     # # SC: O(1)
 
