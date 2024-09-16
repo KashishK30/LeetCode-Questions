82b9511ac50795cdf3387a4faacce0d334976a53
KashishK30 <khobragadekashish@gmail.com>
2024-09-16 16:31:28 +0530

    Time: 133 ms (14.03%), Space: 20.3 MB (94.46%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,15 +1,22 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         n = len(intervals)
-
+        
+        # Sort the elements based on the starting time
         intervals.sort()
 
         ans = []
 
+        # Initialized with 1st interval
         for interval in intervals:
+            # If ans is empty or current element does not overlap, add it to ans
             if not ans or ans[-1][1] < interval[0]:
                 ans.append(interval)
             else:
+                # If ther is an overlap, merge the intervals by updating the end
                 ans[-1][1] = max(ans[-1][1], interval[1])
         return ans
+
+    # TC: O(n)
+    # SC: O(1)
 
