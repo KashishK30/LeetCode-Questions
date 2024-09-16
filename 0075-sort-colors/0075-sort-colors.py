a424b2a953ad3f47e5d051bf64c2afc55b97b0af
KashishK30 <khobragadekashish@gmail.com>
2024-09-16 12:34:05 +0530

    Time: 43 ms (19.04%), Space: 16.5 MB (67.96%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,13 +3,26 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-    # BUBBLE SORT
+    # SELECTION SORT
         n = len(nums)
         for i in range(n):
-            for j in range(n - i - 1):
-                if nums[j] > nums[j + 1]:
-                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+            min_ind = i
+            for j in range(i + 1, n):
+                if nums[j] < nums[min_ind]:
+                    min_ind = j
+
+            nums[i], nums[min_ind] = nums[min_ind], nums[i]
         return nums
+
+    # # BUBBLE SORT
+    #     n = len(nums)
+    #     for i in range(n):
+    #         for j in range(n - i - 1):
+    #             if nums[j] > nums[j + 1]:
+    #                 nums[j], nums[j + 1] = nums[j + 1], nums[j]
+    #     return nums
+    # # TC: O(n ^ 2)
+    # # SC: O(1)
 
     # # Brute Force
 
