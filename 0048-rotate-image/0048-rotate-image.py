28041fde8573764aa3618338109ce771e77bc64d
KashishK30 <khobragadekashish@gmail.com>
2024-09-16 15:53:09 +0530

    Time: 48 ms (6.34%), Space: 16.6 MB (46.53%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -4,9 +4,16 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix)
-        
+
         for i in range(n):
             for j in range(i, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         for i in range(n):
             matrix[i].reverse()
+        
+        return matrix
+        
+    # TC: O(n * n) for transposing
+    # O(n * n) for reversing
+
+    # SC: O(1)
