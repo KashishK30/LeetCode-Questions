83e19cd8fb5c39e5415682056f41319df520b929
KashishK30 <khobragadekashish@gmail.com>
2024-09-16 11:11:22 +0530

    Time: 585 ms (6.18%), Space: 31 MB (70.33%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,45 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # Optimal Solution
+        # KADANE'S Algorithm
+        n = len(nums)
+
+        max_sum = float('-inf')
+        sum_arr = 0
+
+        for i in range(n):
+            sum_arr = max(sum_arr + nums[i], nums[i])
+            max_sum = max(sum_arr, max_sum)
+        return max_sum
+
+        # TC: O(n)
+        # SC: O(1)
+
+        # Better Approach
+        n = len(nums)
+
+        max_sum = float('-inf')
+        for i in range(n):
+            sum_arr = 0
+            for j in range(i, n):
+                sum_arr += nums[j]
+                max_sum = max(max_sum, sum_arr)
+        return max_sum
+
+        # TC: O(n^ 2)
+        # SC: O(1)
+
+        # # Brute Force
+        # n = len(nums)
+
+        # max_sum = float('-inf')
+        # for i in range(n):
+        #     for j in range(i, n):
+        #         sum_arr = 0
+        #         for k in range(i, j + 1):
+        #             sum_arr += nums[k]
+        #         max_sum = max(max_sum, sum_arr)
+        # return max_sum
+
+        # # TC: O(n ^ 3)
+        # # SC: O(1)
