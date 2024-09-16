b16799f4ce884416916e241fd79e5751c10af040
KashishK30 <khobragadekashish@gmail.com>
2024-09-16 11:20:25 +0530

    Time: 613 ms (5.05%), Space: 31 MB (42.41%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -2,34 +2,34 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         # Optimal Solution
         # KADANE'S Algorithm
-        # n = len(nums)
+        n = len(nums)
 
-        # max_sum = float('-inf')
-        # sum_arr = 0
+        max_sum = float('-inf')
+        sum_arr = 0
 
-        # for i in range(n):
-        #     sum_arr = max(sum_arr + nums[i], nums[i])
-        #     max_sum = max(sum_arr, max_sum)
-        # return max_sum
+        for i in range(n):
+            sum_arr = max(sum_arr + nums[i], nums[i])
+            max_sum = max(sum_arr, max_sum)
+        return max_sum
 
         # TC: O(n)
         # SC: O(1)
 
-        # DP Solution
-        n = len(nums)
-        dp = [0] * n
+        # # DP Solution
+        # n = len(nums)
+        # dp = [0] * n
 
-        dp[0] = nums[0]
+        # dp[0] = nums[0]
 
-        max_sum = dp[0]
+        # max_sum = dp[0]
 
-        for i in range(1, n):
-            dp[i] = max(dp[i - 1] + nums[i], nums[i])
-            max_sum = max(dp[i], max_sum)
-        return max_sum
+        # for i in range(1, n):
+        #     dp[i] = max(dp[i - 1] + nums[i], nums[i])
+        #     max_sum = max(dp[i], max_sum)
+        # return max_sum
 
-        # TC: O(n)
-        # SC: O(n)
+        # # TC: O(n)
+        # # SC: O(n)
 
         # Better Approach
         # n = len(nums)
