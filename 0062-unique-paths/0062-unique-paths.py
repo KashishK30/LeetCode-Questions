4c2649ed4dda629b8a3c02de86873fe33ba9b01f
KashishK30 <khobragadekashish@gmail.com>
2024-09-17 18:25:56 +0530

    Time: 33 ms (73.86%), Space: 16.6 MB (15.38%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,18 +1,31 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0 for _ in range(n)] for _ in range(m)]
-
-        if m == 1 or n == 1:
-            return 1
-        
-        for i in range(1, m):
-            dp[i][0] = 1
-        
-        for j in range(1, n):
-            dp[0][j] = 1
+    # SPACE OPTIMIZATION
+        curr = [1] * n
+        prev = [1] * n
 
         for i in range(1, m):
             for j in range(1, n):
-                dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+                curr[j] = prev[j] + curr[j - 1]
+            curr, prev = prev, curr
+        return  prev[n - 1]
 
-        return dp[i][j]
+    # # TABULATION
+    #     dp = [[0 for _ in range(n)] for _ in range(m)]
+
+    #     if m == 1 or n == 1:
+    #         return 1
+        
+    #     for i in range(1, m):
+    #         dp[i][0] = 1
+        
+    #     for j in range(1, n):
+    #         dp[0][j] = 1
+
+    #     for i in range(1, m):
+    #         for j in range(1, n):
+    #             dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+
+    #     return dp[i][j]
+    # # TC: O(m * n)
+    # # SC: O(m * n)
