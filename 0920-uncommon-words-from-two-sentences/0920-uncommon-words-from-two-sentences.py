bc38c15dd5aa434248a3a8a4e9e779030f85a7d6
KashishK30 <khobragadekashish@gmail.com>
2024-09-17 15:00:09 +0530

    Time: 35 ms (59.61%), Space: 16.6 MB (44.21%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.py
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.py
@@ -1,18 +1,39 @@
+from collections import Counter
+from typing import List
+
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
-        string1 = s1.split()
-        string2 = s2.split()
-        word_count = defaultdict(int)
+        words1 = s1.split()
+        words2 = s2.split()
 
-        for word1 in string1:
-            word_count[word1] += 1
-        
-        for word2 in string2:
-            word_count[word2] += 1
-        
-        result = []
+        count1 = Counter(words1)
+        count2 = Counter(words2)
 
-        for word in word_count:
-            if word_count[word] == 1:
-                result.append(word)
+        combined_count = count1 + count2
+
+        result = [word for word, count in combined_count.items() if count == 1]
+
         return result
+
+    # TC: O(m + n) we are iterating through both the strings and processing each word once
+    # SC: O(k) k: no of unique words
+
+        # string1 = s1.split() # TC: O(m); m = len(s1)
+        # string2 = s2.split() # TC: O(n); n = len(s2)
+        # word_count = defaultdict(int) # Building the word dictionary TC: O(m + n)
+
+        # for word1 in string1:
+        #     word_count[word1] += 1
+        
+        # for word2 in string2:
+        #     word_count[word2] += 1
+        
+        # result = [] # TC: O(m + n) in worst case
+
+        # for word in word_count:
+        #     if word_count[word] == 1:
+        #         result.append(word)
+        # return result
+
+        # # TC: O(m + n)
+        # # SC: O(m + n) for string1, string2, word_count and result respectively
