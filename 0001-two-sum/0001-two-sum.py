e5730473b96aac47b7a6dcf1e7f48f29dd4a9253
KashishK30 <khobragadekashish@gmail.com>
2024-09-19 22:45:20 +0530

    Time: 55 ms (80.74%), Space: 17.7 MB (30.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,28 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-    # BRUTE FORCE
-        result = []
+    # BETTER SOLUTION
         n = len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
-                    result.append(i)
-                    result.append(j)
-        return result
-    # TC: O(n ^ 2)
+        num_to_index = {}
+        result = []
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_to_index:
+                return [i, num_to_index[complement]]
+            num_to_index[num] = i
+
+        return[-1, -1]
+    # TC: O(n)
     # SC: O(1)
+
+    # # BRUTE FORCE
+    #     result = []
+    #     n = len(nums)
+    #     for i in range(n):
+    #         for j in range(i + 1, n):
+    #             if nums[i] + nums[j] == target:
+    #                 result.append(i)
+    #                 result.append(j)
+    #     return result
+    # # TC: O(n ^ 2)
+    # # SC: O(1)
