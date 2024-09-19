8c5ac21eb8486f8eb5db85b8900ded5dbd646b26
KashishK30 <khobragadekashish@gmail.com>
2024-09-19 23:13:28 +0530

    Time: 66 ms (42.72%), Space: 18.1 MB (8.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,5 +1,36 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+    # BETTER SOLUTION (Optimal for yes or no question, without printing index)
+        # Step 1: Store the original indices along with the values
+        indexed_arr = list(enumerate(nums))
+
+        # Step 2: Sort based on the values (not indices)
+        indexed_arr.sort(key = lambda x: x[1])
+
+        # Step 3: Two-pointers technique
+        n = len(nums)
+        l = 0
+        r = len(nums) - 1
+
+        while l < r:
+            current_sum = indexed_arr[l][1] + indexed_arr[r][1]
+
+            if current_sum == target:
+                return [indexed_arr[l][0], indexed_arr[r][0]]
+            
+            elif current_sum > target:
+                r -= 1
+            
+            else:
+                l += 1
+            
+        return [-1, -1]
+    # TC: O(n * log n)
+    # SC: O(n)
+
+
+
+
     # BETTER SOLUTION
         n = len(nums)
         num_to_index = {}
