b90c5892c88a29e0160b233f8c4464532c55944b
KashishK30 <khobragadekashish@gmail.com>
2024-09-21 19:26:37 +0530

    Time: 352 ms (63.02%), Space: 31.9 MB (31.89%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,26 +1,62 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-    # BETTER
+    # OPTIMAL
         n = len(nums)
+
         if n == 0:
             return 0
-
-        nums.sort()
-        last_smaller = float('-inf')
-        count = 0
-        longest = 1
         
+        longest = 1
+
+        # Create a set to store all unique numbers in the input list
+        num_set = set()
+
+        # Add all elements from set list to set for O(1) loop
         for i in range(n):
-            if nums[i] == last_smaller + 1:
-                count += 1
-                last_smaller = nums[i]
-            elif nums[i] != last_smaller:
-                count = 1
-                last_smaller = nums[i]
-            longest = max(longest, count)
+            num_set.add(nums[i])
+        
+        # Iterate through each numberr in the set
+        for num in num_set:
+            # Check if the current number is the start of the subsequence
+            # i.e num - 1 should not be in the set
+            if num - 1 not in num_set:
+                # Initialize the count for current subsequence
+                current_sequence_len = 1
+                current_num = num
+
+                # Find all consecutive numbers starting from num
+                while current_num + 1 in num_set:
+                    current_sequence_len += 1
+                    current_num += 1
+                
+                longest = max(longest, current_sequence_len)
         return longest
-    # TC: O(n log n) + O(n) = sorting + 1 for loop
-    # SC: O(1)
+    # TC: O(n) for inserting the elements into set
+    # O(n) each no. in the set is checked at most once
+    # Overall TC: O(n)
+    # SC: O(n)
+
+    # # BETTER
+    #     n = len(nums)
+    #     if n == 0:
+    #         return 0
+
+    #     nums.sort()
+    #     last_smaller = float('-inf')
+    #     count = 0
+    #     longest = 1
+        
+    #     for i in range(n):
+    #         if nums[i] == last_smaller + 1:
+    #             count += 1
+    #             last_smaller = nums[i]
+    #         elif nums[i] != last_smaller:
+    #             count = 1
+    #             last_smaller = nums[i]
+    #         longest = max(longest, count)
+    #     return longest
+    # # TC: O(n log n) + O(n) = sorting + 1 for loop
+    # # SC: O(1)
 
     # BRUTE FORCE
         # n = len(nums)
